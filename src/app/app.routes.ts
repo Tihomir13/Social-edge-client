6f0e65380200483985a9dc592eda6c9cf43b0c85
Tihomir13 <tihomir13n2002@abv.bg>
2024-09-24 16:26:32 +0300

    updating routes for login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'register',
-    },
-    {
-        path: 'register',
-        component: RegisterComponent
-    }
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'register',
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
 ];
