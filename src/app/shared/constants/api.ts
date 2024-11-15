9e4c81f683d30b9b7462626c4fafad44d5c1d502
Tihomir13 <tihomir13n2002@abv.bg>
2024-11-15 19:29:05 +0200

    initial api

--- src/app/shared/constants/api.ts
+++ src/app/shared/constants/api.ts
@@ -1,2 +1,3 @@
-export const api = 'http://localhost:3000';
+// export const api = 'http://localhost:3000';
 // export const api = 'http://192.168.30.163:3000';
+export const api = 'https://social-edge-server.onrender.com';
